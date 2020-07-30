c6c39cbbc244e511d1800bfb7563f70799aee95f
Rushi Chandalia <rushichandalia.ict17@gmail.com>
2020-07-30 17:48:48 +0530

    added truffle and connected it metamask

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainNetwork();
+  }
+  async loadBlockchainNetwork() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    const ethereum = window.ethereum;
+    const enabledWeb3 = await ethereum.enable();
+    const acc = await web3.eth.getAccounts();
+    this.setState({ accounts: acc[0] });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { accounts: "" };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+        <p>Your account: {this.state.accounts}</p>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//   );
+// }
 
 export default App;
